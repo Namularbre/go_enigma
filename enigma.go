4c8ae3957baf831eba60348d7e107593d809689b
Thomas SAZERAT <namulabre@gmail.com>
2023-12-20 16:35:17 +0100

    enigma: Changed variable declaration

--- enigma.go
+++ enigma.go
@@ -11,7 +11,7 @@ type Enigma struct {
 }
 
 func GenerateEnigma() *Enigma {
-	e := Enigma{}
+	var e Enigma
 
 	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 
