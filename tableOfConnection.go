19967fc74593f5f34ccf2d23734931e3daa9b436
Thomas SAZERAT <namulabre@gmail.com>
2023-12-21 01:32:32 +0100

    tableOfConnection: If a char is unknow, it's ignored

--- tableOfConnection.go
+++ tableOfConnection.go
@@ -1,14 +1,18 @@
 package main
 
+import "slices"
+
 type TableOfConnection struct {
 	input  []string
 	output []string
 }
 
 func (toc *TableOfConnection) GetOutput(i string) string {
-	for index, char := range toc.input {
-		if char == i {
-			return toc.output[index]
+	if slices.Contains(toc.input, i) {
+		for index, char := range toc.input {
+			if char == i {
+				return toc.output[index]
+			}
 		}
 	}
 	return ""
