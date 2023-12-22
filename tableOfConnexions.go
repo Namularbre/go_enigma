5ee59440fbcaedcd3f551b9d08903f62fc0d8ea8
Thomas SAZERAT <namulabre@gmail.com>
2023-12-22 22:58:22 +0100

    tableOfConnexions: Moved the generation function in file

--- tableOfConnexions.go
+++ tableOfConnexions.go
@@ -14,3 +14,10 @@ func (toc *TableOfConnexions) GetOutput(i string) string {
 	}
 	return ""
 }
+
+func GenerateTableOfConnexions() *TableOfConnexions {
+	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+	changedAlphabet := []string{"a", "c", "i", "e", "f", "g", "h", "j", "k", "l", "x", "y", "o", "w", "p", "q", "r", "d", "s", "b", "t", "u", "v", "m", "z", "n"}
+	return &TableOfConnexions{input: alphabet, output: changedAlphabet}
+}
