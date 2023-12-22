aec44f9030186ee41ccae53bdf23577a904be684
Thomas SAZERAT <namulabre@gmail.com>
2023-12-22 22:20:14 +0100

    rotor: Added the rotor rotation, fixed the next rotor retations

--- rotor.go
+++ rotor.go
@@ -13,15 +13,12 @@ func (r *Rotor) rotate() {
 	r.gap++
 	if r.gap == len(r.output) {
 		r.gap = 0
-		for _, next := range r.nexts {
-			next.rotate()
-		}
+		r.nexts[0].rotate()
 	}
 }
 
 func (r *Rotor) GetOutput(i string) string {
 	if slices.Contains(r.input, i) {
-
 		inputIndex := slices.Index(r.input, i)
 		outputIndex := (inputIndex+r.gap)%len(r.output) - 1
 		output := r.output[outputIndex]
@@ -29,6 +26,7 @@ func (r *Rotor) GetOutput(i string) string {
 		for _, next := range r.nexts {
 			output = next.GetOutput(output)
 		}
+		r.rotate()
 
 		return output
 	}
